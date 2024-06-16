a65dd43473be86ea58e4b3b8d4ee2243f7323680
Rodolfo P. Perez III <iampereziii@gmail.com>
2024-06-17 03:00:02 +0800

    setup db

--- AICommerceHub.Data/Context/AICommerceHub.Context.cs
+++ AICommerceHub.Data/Context/AICommerceHub.Context.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 namespace AICommerceHub.Data.Context
 {
-    public class AICommerceHub : DbContext // TODO replace to identity later on
+    public class AICommerceHubContext : IdentityDbContext // TODO replace to identity later on
     {
-
+        public AICommerceHubContext(DbContextOptions<AICommerceHubContext> options)
+        : base(options)
+        {
+        }
 
     }
 }
