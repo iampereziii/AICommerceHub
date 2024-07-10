7413208bc8d0e596760ffce4d43a56a6061dadc0
Rodolfo P. Perez III <iampereziii@gmail.com>
2024-07-11 03:11:03 +0800

    interface for the AI service

---
+++ AICommerceHub.Service/Contract/IAiService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AICommerceHub.Service.Contract
+{
+    internal interface IAiService
+    {
+        String GenerateHTML(String topic);
+        void ModifyExistingHtml(String topic);
+        String GetExistinHTML(String topic);
+
+    }
+}
