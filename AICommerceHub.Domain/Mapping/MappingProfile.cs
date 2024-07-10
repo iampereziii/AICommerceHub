89ea75e7f2f504a8e100a83b27708fb7565866cf
Rodolfo P. Perez III <iampereziii@gmail.com>
2024-07-11 02:23:05 +0800

    add auto mapper

---
+++ AICommerceHub.Domain/Mapping/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AICommerceHub.Domain.DTOs;
+using AICommerceHub.Domain.Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AICommerceHub.Domain.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<OrderDTO, Order>();
+            CreateMap<Order,OrderDTO>();
+
+            CreateMap<Product, ProductDTO>();
+            CreateMap<ProductDTO, Product>();
+
+        }
+    }
+}
